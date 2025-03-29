9c90612dfa9867f2bad8ff95faf1283f4c1c8203
foxngao <minhtienst0@gmail.com>
2025-03-29 10:39:28 +0700

    Sửa đổi AddContactForm

--- ContactList/AddContactForm.Designer.cs
+++ ContactList/AddContactForm.Designer.cs
@@ -32,6 +32,7 @@
             this.txtPhone = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtName
@@ -67,11 +68,21 @@
             this.textBox1.TabIndex = 3;
             this.textBox1.Text = "Email";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(417, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Thông tin bổ sung";
+            // 
             // AddContactForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.txtPhone);
@@ -89,5 +100,6 @@
         private System.Windows.Forms.TextBox txtPhone;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
